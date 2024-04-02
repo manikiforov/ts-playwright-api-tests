cdeef723fbd790747926d43fb9bd108659b19abe
maksim.nikiforov <pusyash91@yandex.ru>
2024-04-02 15:30:25 -0300

    'Lesson 57. Perform API requests' completed

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, request } from '@playwright/test'
 import tags from '../test-data/tags.json'
 
 test.beforeEach(async ({page}) => {
@@ -10,12 +10,22 @@ test.beforeEach(async ({page}) => {
     })
   })
 
+  await page.goto('https://conduit.bondaracademy.com/')
+  await page.getByText('Sign In').click()
+  await page.getByRole('textbox', {name: "Email"}).fill("maxtest@test.com")
+  await page.getByRole('textbox', {name: "Password"}).fill("welcome1")
+  await page.getByRole('button').click()
+
+})
+
+test('has title', async ({ page }) => {
+
   await page.route('*/**/api/articles*', async route => {
 
     const response = await route.fetch()
     const responseBody = await response.json()
-    responseBody.articles[0].title = "This is test title"
-    responseBody.articles[0].description = "This is test description"
+    responseBody.articles[0].title = "This is a MOCK test title"
+    responseBody.articles[0].description = "This is a MOCK test description"
 
     await route.fulfill({
       body: JSON.stringify(responseBody)
@@ -23,16 +33,46 @@ test.beforeEach(async ({page}) => {
 
   })
 
-
-  await page.goto('https://conduit.bondaracademy.com/')
+  await page.getByText('Global Feed').click()
+  
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title')
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK test description')
+  await page.waitForTimeout(500)
 
 })
 
-test('has title', async ({ page }) => {
+test('delete article', async({page, request}) => {
 
-  await expect(page.locator('.navbar-brand')).toHaveText('conduit')
-  await expect(page.locator('app-article-list h1').first()).toContainText('This is test title')
-  await expect(page.locator('app-article-list p').first()).toContainText('This is test description')
-  await page.waitForTimeout(500)
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {
+            "user": {email: "maxtest@test.com", password: "welcome1"}
+    }
+  })
 
-});
+  const responseBody = await response.json()
+  const accessToken = responseBody.user.token
+
+  const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
+    data: {
+      article: {
+        body: "This is a test body",
+        description: "This is a test Headline",
+        tagList: [],
+        title: "This is a test article"
+      }
+    },
+    headers: {
+      Authorization: `Token ${accessToken}`
+    }
+  })
+  expect(articleResponse.status()).toEqual(201)
+
+  await page.getByText('Global Feed').click()
+  await page.getByText('This is a test article').click()
+  await page.getByRole('button', {name: "Delete Article"}).first().click()
+  await page.getByText('Global Feed').click()
+
+  await expect(page.locator('app-article-list h1').first()).not.toContainText('This is a test article')
+
+})
