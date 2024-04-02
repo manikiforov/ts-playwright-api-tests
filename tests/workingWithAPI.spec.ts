8336f4f0022b7e898dfdd165189fc93c99f3714d
maksim.nikiforov <pusyash91@yandex.ru>
2024-04-02 16:16:13 -0300

    'Lesson 59. Sharing Authentication state' completed

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -11,10 +11,10 @@ test.beforeEach(async ({page}) => {
   })
 
   await page.goto('https://conduit.bondaracademy.com/')
-  await page.getByText('Sign In').click()
-  await page.getByRole('textbox', {name: "Email"}).fill("maxtest@test.com")
-  await page.getByRole('textbox', {name: "Password"}).fill("welcome1")
-  await page.getByRole('button').click()
+  // await page.getByText('Sign In').click()
+  // await page.getByRole('textbox', {name: "Email"}).fill("maxtest@test.com")
+  // await page.getByRole('textbox', {name: "Password"}).fill("welcome1")
+  // await page.getByRole('button').click()
 
 })
 
